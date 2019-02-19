a1b49e968307409b630f5dc391aa05728e3f5dcd
Enrico Bertolazzi <enrico.bertolazzi@unitn.it>
2019-02-19 21:26:06 +0100

    improved path search

--- mrblib/require.rb
+++ mrblib/require.rb
@@ -2,15 +2,50 @@ class LoadError < ScriptError; end
 
 module Kernel
 
+  def ___file_search( path, exts )
+
+    raise TypeError unless path.class == String
+
+    filenames = [path]
+    if not ( exts.include? File.extname(path) ) then
+      exts.each do |e| filenames << path+e end
+    end
+
+    path0 = nil
+    if ['/', '.'].include? path[0] or not $LOAD_PATH
+      path0 = filenames.find do |fname| File.file? fname end
+    else
+      found = false
+      filenames.find do |fname|
+        $LOAD_PATH.each do |dir0|
+          path0 = File.join dir0, fname
+          found = File.file? path0
+          break if found
+        end
+        break if found
+      end
+    end
+
+    unless path0 && File.file?(path0)
+      raise LoadError,
+            "cannot load such file (bad path or not a file) -- #{path}"
+    end
+
+    return File.realpath(path0)
+  end
+
   def load(path)
     raise TypeError unless path.class == String
 
-    if File.exist?(path) && File.extname(path) == ".mrb"
-      _load_mrb_file path
-    elsif File.exist?(path)
-      _load_rb_str File.open(path).read.to_s, path
+    # require method can load .rb, .mrb or without-ext filename only.
+    realpath = ___file_search( path,  [ ".rb", ".mrb", ".so", ".dylib", ".dll"] )
+
+    if File.extname(realpath) == ".mrb"
+      ___load_mrb_file realpath
+    elsif File.extname(realpath) == ".rb"
+      ___load_rb_str File.open(realpath).read.to_s, realpath
     else
-      raise LoadError, "File not found -- #{path}"
+      ___load_shared_file realpath
     end
 
     true
@@ -20,48 +55,23 @@ module Kernel
     raise TypeError unless path.class == String
 
     # require method can load .rb, .mrb or without-ext filename only.
-    filenames = []
-    if [".rb", ".mrb"].include? File.extname(path) then
-      filenames << path
-    else
-      filenames << "#{path}.rb"
-      filenames << "#{path}.mrb"
-    end
-
-    dir = nil
-    filename = nil
-    if ['/', '.'].include? path[0]
-      path0 = filenames.find do |fname|
-        File.file?(fname)
-      end
-    else
-      dir = ($LOAD_PATH || []).find do |dir0|
-        filename = filenames.find do |fname|
-          path0 = File.join dir0, fname
-          File.file?(path0)
-        end
-      end
-      path0 = dir && filename ? File.join(dir, filename) : nil
-    end
-
-    unless path0 && File.file?(path0)
-      raise LoadError, "cannot load such file (bad path or not a file) -- #{path}"
-    end
-
-    realpath = File.realpath(path0)
+    realpath = ___file_search( path,  [".rb", ".mrb"] )
 
     # already required
     return false if ($" + $__mruby_loading_files__).include?(realpath)
 
     $__mruby_loading_files__ << realpath
-    load realpath
+    if File.extname(realpath) == ".mrb"
+      ___load_mrb_file realpath
+    else
+      ___load_rb_str File.open(realpath).read.to_s, realpath
+    end
     $" << realpath
     $__mruby_loading_files__.delete realpath
 
     true
   end
 end
-
 
 $LOAD_PATH ||= []
 $LOAD_PATH << '.'
--- src/require.c
+++ src/require.c
@@ -3,10 +3,14 @@
 #endif
 
 #include "mruby.h"
-#include "mruby/compile.h"
-#include "mruby/dump.h"
+#include "mruby/data.h"
 #include "mruby/string.h"
+#include "mruby/dump.h"
 #include "mruby/proc.h"
+#include "mruby/compile.h"
+#include "mruby/variable.h"
+#include "mruby/array.h"
+#include "mruby/numeric.h"
 
 #include "opcode.h"
 #include "error.h"
@@ -18,10 +22,15 @@
   #include <unistd.h>
 #endif
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdlib.h>
+#include <limits.h>
 #include <fcntl.h>
 #include <setjmp.h>
-#include <stdlib.h>
 #include <sys/stat.h>
+#include <sys/types.h>
+
 
 #define E_LOAD_ERROR (mrb_class_get(mrb, "LoadError"))
 
@@ -33,11 +42,11 @@
 #if MRUBY_RELEASE_NO < 10000
   mrb_value
   mrb_yield_internal(
-    mrb_state *mrb,
-    mrb_value b,
-    int       argc,
-    mrb_value *argv,
-    mrb_value self,
+    mrb_state * mrb,
+    mrb_value   b,
+    int         argc,
+    mrb_value * argv,
+    mrb_value   self,
     struct RClass *c
   );
   #define mrb_yield_with_class mrb_yield_internal
@@ -68,7 +77,7 @@
 #ifdef USE_MRUBY_OLD_BYTE_CODE
   static
   void
-  replace_stop_with_return( mrb_state *mrb, mrb_irep *irep ) {
+  replace_stop_with_return( mrb_state * mrb, mrb_irep * irep ) {
     if (irep->iseq[irep->ilen - 1] == MKOP_A(OP_STOP, 0)) {
       irep->iseq = mrb_realloc(mrb, irep->iseq, (irep->ilen + 1) * sizeof(mrb_code));
       irep->iseq[irep->ilen - 1] = MKOP_A(OP_LOADNIL, 0);
@@ -218,13 +227,13 @@ mrb_require_load_rb_str(
 
 static
 mrb_value
-mrb_require_load_mrb_file( mrb_state *mrb, mrb_value self ) {
+mrb_require_load_mrb_file( mrb_state * mrb, mrb_value self ) {
   char *path_ptr = NULL;
   FILE *fp       = NULL;
   mrb_irep *irep;
   mrb_value path;
 
-  mrb_get_args(mrb, "S", &path);
+  mrb_get_args( mrb, "S", &path );
   path_ptr = mrb_str_to_cstr( mrb, path );
 
   #ifdef OS_WINDOWS
@@ -250,18 +259,249 @@ mrb_require_load_mrb_file( mrb_state *mrb, mrb_value self ) {
   return mrb_true_value();
 }
 
+/*
+============================================================
+============================================================
+============================================================
+*/
+
+static
+char const *
+file_basename( char const fname[] ) {
+  char const * tmp ;
+  char const * ptr ;
+
+  // search last / or \\ character
+  ptr = tmp = fname ;
+  while ( tmp ) {
+    if ( (tmp = strchr(ptr, '/' )) ||
+         (tmp = strchr(ptr, '\\')) ) ptr = tmp + 1;
+  }
+
+  return ptr ;
+}
+
+#if defined(_MSC_VER) || defined(__MINGW32__)
+  #ifndef PATH_MAX
+    #define PATH_MAX MAX_PATH
+  #endif
+  #define strdup(x) _strdup(x)
+#else
+  #include <sys/param.h>
+  #include <unistd.h>
+  #include <libgen.h>
+  #include <dlfcn.h>
+#endif
+
+#ifndef MAXPATHLEN
+  #define MAXPATHLEN 1024
+#endif
+
+#ifndef MAXENVLEN
+  #define MAXENVLEN 1024
+#endif
+
+#ifdef OS_WINDOWS
+
+  #include <windows.h>
+
+  static
+  int
+  relativeToFullPath( char const path[], char full_path[MAXPATHLEN] ) {
+    DWORD retval = GetFullPathNameA( path, MAXPATHLEN, full_path, NULL );
+    return retval > 0 && retval < MAXPATHLEN ;
+  }
+
+  static
+  int
+  GetEnvironmentToString( char const envName[], char out[], unsigned len ) {
+    DWORD n = GetEnvironmentVariableA(envName,out,(DWORD)len);
+    return n > 0 && n < (DWORD)len ;
+  }
+
+  void
+  CheckError( char const lib[], mrb_state * mrb ) {
+    // Get the error message, if any.
+    DWORD errorMessageID = GetLastError();
+    if ( errorMessageID == 0 ) return ; // No error message has been recorded
+    printf("errorMessageID: %ld\n", errorMessageID);
+
+    LPSTR messageBuffer = NULL;
+    size_t size = FormatMessageA( FORMAT_MESSAGE_ALLOCATE_BUFFER |
+                                  FORMAT_MESSAGE_FROM_SYSTEM |
+                                  FORMAT_MESSAGE_IGNORE_INSERTS,
+                                  NULL,
+                                  errorMessageID,
+                                  MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+                                  (LPSTR)&messageBuffer,
+                                  0,
+                                  NULL );
+
+    printf("failed to load DLL: %s\n", lib);
+    mrb_raise( mrb, E_RUNTIME_ERROR, messageBuffer ) ;
+    // Free the buffer.
+    LocalFree(messageBuffer);
+  }
+
+#else
+
+  #include <dlfcn.h>
+
+  static
+  int
+  relativeToFullPath( char const path[], char full_path[MAXPATHLEN] ) {
+    return realpath(path, full_path) != NULL ;
+  }
+
+  static
+  int
+  GetEnvironmentToString( char const envName[], char out[], unsigned len ) {
+    char const * ptr = getenv( envName ) ;
+    int ok = ptr != NULL && strlen( ptr ) < len ;
+    if ( ok ) strcpy( out, ptr ) ;
+    return ok ;
+  }
+
+  void
+  CheckError( char const lib[], mrb_state * mrb ) {
+    char const * err = dlerror() ;
+    if ( err != NULL ) {
+      printf("failed to load DLL: %s\n", lib);
+      mrb_raise( mrb, E_RUNTIME_ERROR, dlerror() );
+    }
+  }
+
+#endif
+
+// workaround for new mruby version
+#if MRUBY_RELEASE_MAJOR <= 1 && MRUBY_RELEASE_MINOR <= 3
+  #define TARGET_CLASS(PROC) PROC->target_class
+#else
+  #define TARGET_CLASS(PROC) PROC->e.target_class
+#endif
+
+static
+void
+mrb_load_irep_data( mrb_state * mrb, const uint8_t* data ) {
+
+  //printf( "require:mrb_load_irep_data\n") ;
+
+  int ai = mrb_gc_arena_save(mrb);
+  mrb_irep *irep = mrb_read_irep(mrb,data);
+  mrb_gc_arena_restore(mrb,ai);
+
+  if (irep) {
+    #ifdef USE_MRUBY_OLD_BYTE_CODE
+    replace_stop_with_return(mrb, irep);
+    #endif
+    struct RProc *proc = mrb_proc_new(mrb, irep);
+    TARGET_CLASS(proc) = mrb->object_class; // changed RProc with a union
+
+    int ai = mrb_gc_arena_save(mrb);
+    mrb_yield_with_class( mrb,
+                          mrb_obj_value(proc),
+                          0,
+                          NULL,
+                          mrb_top_self(mrb),
+                          mrb->object_class );
+    mrb_gc_arena_restore(mrb, ai);
+  } else if (mrb->exc) {
+    // fail to load
+    longjmp(*(jmp_buf*)mrb->jmp, 1);
+  }
+}
+
+mrb_value
+mrb_require_load_file( mrb_state * mrb, mrb_value self ) {
+  char entry[PATH_MAX]      = {0};
+  char entry_irep[PATH_MAX] = {0};
+  typedef void (*fn_mrb_gem_init)(mrb_state *mrb);
+  mrb_value mrb_filepath;
+
+  mrb_get_args( mrb, "S", &mrb_filepath );
+  char const * filepath = mrb_str_to_cstr( mrb, mrb_filepath );
+
+  //printf( "require:load_so_file: %s\n", filepath) ;
+
+  #ifdef OS_WINDOWS
+  HMODULE handle = LoadLibrary(filepath);
+  #else
+  void * handle = dlopen(filepath, RTLD_LAZY|RTLD_GLOBAL);
+  #endif
+
+  if ( handle == NULL ) {
+    //printf( "require:load_so_file: null handle, check error\n" ) ;
+    CheckError( filepath, mrb ) ;
+    char message[1024] ;
+    snprintf( message, 1023, "failed to load %s, open return a NULL pointer\n", filepath );
+    printf( "%s", message ) ;
+    mrb_raise(mrb, E_LOAD_ERROR, message );
+  }
+
+  char * ptr = strdup(file_basename(filepath)) ;
+  char * tmp = strrchr(ptr, '.');
+  if (tmp) *tmp = 0;
+  for ( tmp = ptr ; *tmp ; ++tmp ) { if (*tmp == '-') *tmp = '_' ; }
+
+  snprintf(entry,      sizeof(entry)-1,      "mrb_%s_gem_init",    ptr);
+  snprintf(entry_irep, sizeof(entry_irep)-1, "gem_mrblib_irep_%s", ptr);
+  free(ptr);
+
+  //printf( "require:load_so_file attach entry\n") ;
+
+  #ifdef OS_WINDOWS
+  FARPROC addr_entry      = GetProcAddress(handle, entry);
+  FARPROC addr_entry_irep = GetProcAddress(handle, entry_irep);
+  #else
+  void * addr_entry      = dlsym(handle, entry);
+  void * addr_entry_irep = dlsym(handle, entry_irep);
+  #endif
+
+  if ( addr_entry == NULL && addr_entry_irep == NULL ) {
+    char message[1024] ;
+    snprintf( message, 1023, "failed to attach %s or %s in library %s\n",
+              entry, entry_irep, filepath );
+    printf( "%s", message ) ;
+    mrb_raise(mrb, E_LOAD_ERROR, message );
+  }
+
+  if ( addr_entry != NULL ) {
+    printf( "Attach %s from library %s\n", entry, filepath );
+    fn_mrb_gem_init fn = (fn_mrb_gem_init) addr_entry;
+    int ai = mrb_gc_arena_save(mrb);
+    fn(mrb);
+    mrb_gc_arena_restore(mrb, ai);
+  }
+
+  if ( addr_entry_irep != NULL ) {
+    printf( "Attach %s from library %s\n", entry_irep, filepath );
+    uint8_t const * data = (uint8_t const *) addr_entry_irep;
+    mrb_load_irep_data( mrb, data );
+  }
+
+  return mrb_true_value();
+}
+
 void
 mrb_pins_mruby_require_gem_init( mrb_state * mrb ) {
   struct RClass *krn;
   krn = mrb->kernel_module;
+
   mrb_define_method(
-    mrb, krn, "_load_rb_str",
+    mrb, krn, "___load_rb_str",
     mrb_require_load_rb_str,
     MRB_ARGS_ANY()
   );
+
   mrb_define_method(
-    mrb, krn, "_load_mrb_file",
+    mrb, krn, "___load_mrb_file",
     mrb_require_load_mrb_file,
+    MRB_ARGS_REQ(1)
+  );
+
+  mrb_define_method(
+    mrb, krn, "___load_shared_file",
+    mrb_require_load_file,
     MRB_ARGS_REQ(1)
   );
 }
